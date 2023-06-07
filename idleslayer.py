fb436888115944619b79b24a2f33d163647936ab
Carlos <carlosjimenezd00@gmail.com>
2023-06-07 04:15:24 +0100

    añadiendo idleslayer.py

---
+++ idleslayer.py
@@ -0,0 +1,47 @@
+from pyautogui import *
+import pyautogui
+import time
+import win32api, win32com
+import random
+import subprocess
+
+counter = 0
+
+#Search and open the game with opensteam.py
+subprocess.call(["python", "Documents\Programas python\opensteam.py"])
+
+time.sleep(4)
+
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    pyautogui.click()
+    time.sleep(random.uniform(0.1,0.5))
+
+#Main menu click
+click(815,22)
+
+time.sleep(5)
+
+#AFK earned coins and slayer points click
+click(815,22)
+
+#Minions menu
+click(129,92)
+
+click(500,979)
+
+upgradePos = 376
+
+time.sleep(0.5)
+
+win32api.SetCursorPos((736, upgradePos))
+
+#This loops will reclaim the rewards from the minions and send them again on a mission if you have the required amount of slayer points
+while counter < 3:
+    win32api.SetCursorPos((736, upgradePos))
+    click(736, upgradePos)
+    counter += 1
+    upgradePos += 274
+    time.sleep(0.5)
+    if counter == 3:
+        break
