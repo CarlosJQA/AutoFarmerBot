d4c4e90a78d8e702ad20fc3140e5846cdfd0d24c
CarlosJQA <carlosjimenezd00@gmail.com>
2023-11-01 16:38:26 +0000

    update idleon script

--- idleon.py
+++ idleon.py
@@ -3,8 +3,9 @@ import pyautogui
 import time
 import win32api, win32com
 import random
-import subprocess
-
+import subprocess 
+import keyboard
+from PIL import Image
 counter = 0
 
 def holdclick(x,y):
@@ -21,6 +22,7 @@ nombreJuego = ("Legends of Idleon MMO")
 steam_path = "C:\Program Files (x86)\Steam\steam.exe"
 
 command = [steam_path]
+
 #search and open the game from steam library
 subprocess.Popen(command)
 
@@ -35,15 +37,27 @@ pyautogui.typewrite(nombreJuego)
 time.sleep(0.5)
 
 doubleclick(78,244)
+
 #waiting for servers 
 time.sleep(20)
+
 #set to windowed full screen
 click(1364, 249)
 time.sleep(2)
+
 #select elemental sorcerer char
 click(659, 786)
+
 #press play
 click(1760,760)
 time.sleep(random.uniform(4.5, 5.6))
-#claim afk gains
-click(1137,640)
+
+#load claim button image for image recognition
+image = Image.open('claim.PNG').convert('L')
+
+#search image on screen
+location = pyautogui.locateCenterOnScreen('claim.PNG')
+
+#check if the image was found then click on image location
+if location is not None:
+    click(location[0], location[1])
